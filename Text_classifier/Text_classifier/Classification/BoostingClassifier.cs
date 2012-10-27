4c04b239207ef33de99e58c2aae2a97273e3baeb
Sebastian Kruse <s.t.kruse@gmx.de>
2012-10-27 18:36:37 +0200

    Changed boosting classifier and and changed gitignore

--- Text_classifier/Text_classifier/Classification/BoostingClassifier.cs
+++ Text_classifier/Text_classifier/Classification/BoostingClassifier.cs
@@ -14,6 +14,47 @@ namespace Text_classifier.Classification
         {
             this.subclassifiers.Add(classifier);
         }
+        /*
+        void IClassifier.Train(string text1, string text2)
+        {
+            if (this.subclassifiers.Count == 0)
+                throw new Exception("No subclassifiers for boosting");
+            this.weights.RemoveAll(e => true);
+            foreach (var subclassfier in this.subclassifiers)
+                subclassfier.Train(text1, text2);
+
+            var samples = Utils.ExtractSamples(text1).Select(s => new SamplePoint() { Text = s, Class = -1 }).ToList()
+                .Concat(Utils.ExtractSamples(text2).Select(s => new SamplePoint() { Text = s, Class = 1 }).ToList()).ToArray();
+
+            var loss = new double[samples.Count()];
+            for (int i = 0; i < this.subclassifiers.Count(); i++)
+            {
+                var subclassifier = subclassifiers[i];
+                double error = 0d;
+                int debugError = 0;
+                for (int j = 0; j < samples.Count(); j++)
+                {
+                    var sample = samples[j];
+                    double estimate = subclassifier.Classify(sample.Text);
+                    if (i == 0)
+                        loss[j] = 1d / samples.Count();
+                    else
+                        loss[j] = loss[j] * Math.Exp(-sample.Class * weights.Last() * estimate);
+                    error += Math.Abs((estimate - sample.Class) / 2) * loss[j];
+                    if (Math.Sign(estimate) != sample.Class) 
+                        debugError++;
+                }
+                weights.Add(ComputeWeight(error));
+                Console.WriteLine("Classifier " + i + " made " + debugError + "/" + samples.Count() + " errors ("+error+"), weight: " + weights.Last());
+            }
+
+        }
+
+        private double ComputeWeight(double error)
+        {
+            return Math.Log((1-error)/error) / 2;
+        }
+        */
 
         void IClassifier.Train(string text1, string text2)
         {
@@ -26,47 +67,33 @@ namespace Text_classifier.Classification
             var samples = Utils.ExtractSamples(text1).Select(s => new SamplePoint() { Text = s, Class = -1 }).ToList()
                 .Concat(Utils.ExtractSamples(text2).Select(s => new SamplePoint() { Text = s, Class = 1 }).ToList()).ToArray();
 
-            // Compute the first weight.
-            var loss = new double[samples.Count()];
-            for (int i = 0; i < loss.Count(); i++)
-                loss[i] = 1d / loss.Count();
-            double error = 0d;
-            int debugError = 0;
-            for (int i = 0; i < samples.Count(); i++)
+            // Calculate squared errors for each classifier.
+            double[] squaredErrors = new double[subclassifiers.Count];
+            int i = 0;
+            foreach (var subclassifier in subclassifiers)
             {
-                double estimate = subclassifiers[0].Classify(samples[i].Text);
-                error += Math.Abs((estimate - samples[i].Class) / 2) * loss[i];
-                if (Math.Sign(estimate) != samples[i].Class) debugError++;
-            }
-            weights.Add(ComputeWeight(error));
-            Console.WriteLine("Classifier 0 made " + debugError + "/" + samples.Count() + " errors");
-
-            // Compute the remaining weights.
-            for (int i = 1; i < this.subclassifiers.Count(); i++)
-            {
-                var subclassifier = subclassifiers[i];
-                error = 0d;
-                debugError = 0;
-                for (int j = 0; j < samples.Count(); j++)
+                double classifierErrors = 0d;
+                foreach (var samplePoint in samples)
                 {
-                    var sample = samples[j];
-                    double estimate = subclassifier.Classify(sample.Text);
-                    loss[j] = loss[j] * Math.Exp(-sample.Class * weights.Last() * estimate);
-                    error += Math.Abs((estimate - sample.Class) / 2) * loss[j];
-                    if (Math.Sign(estimate) != sample.Class) 
-                        debugError++;
+                    var estimate = subclassifier.Classify(samplePoint.Text);
+                    var error = samplePoint.Class - estimate;
+                    classifierErrors += error * error;
                 }
-                weights.Add(ComputeWeight(error));
-                Console.WriteLine("Classifier " + i + " made " + debugError + "/" + samples.Count() + " errors");
+                squaredErrors[i++] = classifierErrors;
             }
 
+            // Weight them by their error rate.
+            double errorSum = squaredErrors.Sum();
+            foreach (var error in squaredErrors)
+            {
+                // smoothen the error share to avoid division by zero
+                double errorShare = (error + 1) / (errorSum + squaredErrors.Count());
+                double weight = (1 - errorShare) / errorShare;
+                Console.WriteLine(errorShare + "->" + weight);             
+                weights.Add(weight);
+            }
         }
-
-        private double ComputeWeight(double error)
-        {
-            return Math.Log((1 - error) / error) / 2;
-        }
-
+   
 
         double IClassifier.Classify(string sample)
         {
@@ -78,8 +105,12 @@ namespace Text_classifier.Classification
             {
                 var weight = this.weights[i];
                 var subclassifier = this.subclassifiers[i];
-                result += weight * subclassifier.Classify(sample);
+                var subresult = subclassifier.Classify(sample);
+                result += weight * subresult;
+                Console.WriteLine("Classifier " + i + " voted: " + subresult);
             }
+            result /= weights.Sum();
+            Console.WriteLine("Overall result: " + result);
             return result;
         }
 
